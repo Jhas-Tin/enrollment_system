c7543b58577885c39bd05c478c6a3135a797a358
Jhas-Tin <jhastingithub@gmail.com>
2025-12-27 02:14:27 +0800

    0

--- db.php
+++ db.php
@@ -1,7 +1,14 @@
 <?php
-$conn = mysqli_connect("db.fr-pari1.bengt.wasmernet.com", "ece82a27730b8000a28d880c4ce9", "0694ece8-2a27-7456-8000-fa8abae7efec", "school_enrollment");
+$host = "db.fr-pari1.bengt.wasmernet.com";
+$username = "ece82a27730b8000a28d880c4ce9";
+$password = "0694ece8-2a27-7456-8000-fa8abae7efec";
+$dbname = "dbcRdnh4W34AHWwhvtFt4Hea"; // Replace this correctly!
+
+$conn = mysqli_connect($host, $username, $password, $dbname);
+
 if (!$conn) {
     die("Database connection failed: " . mysqli_connect_error());
 }
 
+echo "Connected Successfully!";
 ?>

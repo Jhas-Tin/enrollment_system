88c94f130ad86b3a92a29087a2c886ed77a90c59
Jhas-Tin <jhastingithub@gmail.com>
2025-12-27 02:25:39 +0800

    00

--- db.php
+++ db.php
@@ -1,16 +1,14 @@
 <?php
-
-$host = "db.fr-pari1.bengt.wasmernet.com"; // correct spelling: pari1
-$port = "10272"; 
+$host = "db.fr-pari1.bengt.wasmernet.com";
+$port = 10272;
 $username = "ece82a27730b8000a28d880c4ce9";
 $password = "0694ece8-2a27-7456-8000-fa8abae7efec";
-$dbname = "dbcRdnh4W34AHWwhvtFt4Hea"; // must match Wasmer DB name
+$dbname = "dbcRdnh4W34AHWwhvtFt4Hea"; 
 
 $conn = mysqli_connect($host, $username, $password, $dbname, $port);
 
 if (!$conn) {
     die("Database connection failed: " . mysqli_connect_error());
 }
-
 echo "Connected Successfully!";
 ?>

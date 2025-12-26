0dea38bb8201a93952cba05ae19d3e237c842653
Jhas-Tin <jhastingithub@gmail.com>
2025-12-27 02:11:08 +0800

    trying to deploy the repo

--- db.php
+++ db.php
@@ -1,6 +1,7 @@
 <?php
-$conn = mysqli_connect("localhost", "root", "", "school_enrollment");
+$conn = mysqli_connect("db.fr-pari1.bengt.wasmernet.com", "ece82a27730b8000a28d880c4ce9", "0694ece8-2a27-7456-8000-fa8abae7efec", "school_enrollment");
 if (!$conn) {
     die("Database connection failed: " . mysqli_connect_error());
 }
+
 ?>
